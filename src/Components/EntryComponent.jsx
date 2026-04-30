9b9474c82663d3c3042ae8831b6f73e93d859505
Muhammad <iamm.ahsanmurtaza@gmail.com>
2026-04-30 16:53:18 +0500

    Counter is done here

--- src/Components/EntryComponent.jsx
+++ src/Components/EntryComponent.jsx
@@ -1,12 +1,16 @@
 import Header from './Header.jsx'
 import Main from './Main.jsx'
 import Forms from './Forms.jsx'
+import Counter from './Counter.jsx'
+import Aap from './Aap.jsx'
 export default function EntryComponent () {
   return (
     <>
       <Header />
       <Main />
       <Forms />
+      <Counter />
+      <Aap />
     </>
   )
 }
