78b9022dc0e01d80941781457f5fb168f728d047
Muhammad <iamm.ahsanmurtaza@gmail.com>
2026-04-30 16:53:29 +0500

    Pads Project

---
+++ src/Components/Count.jsx
@@ -0,0 +1,9 @@
+export default function Count (prop) {
+  console.log('Hello Count')
+  return (
+    <>
+      <h2>{prop.count}</h2>
+      {/* <h2>{Math.floor(Math.random()* 100)}</h2> */}
+    </>
+  )
+}
